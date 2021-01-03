37bb7a5d397c5e4bdadbebe9460a3527b9a93110
spamm <spamm@59.8.138.131>
2021-01-03 18:22:27 +0900

    UserVo IUserDAO BoardVO 틀 잡고 시작 CSS JS 는 아직 배우고 있는중입니다.

---
+++ Wow_Family/src/kr/co/wow/IUserDAO.java
@@ -0,0 +1,27 @@
+package kr.co.wow;
+
+public interface IUserDAO {
+
+	// 유저 입력
+	void userAdd(String id);
+	
+	// 유저 삭제
+	void userDel(String id);
+	
+	// 유저 수정(아이디, 비밀번호 제외)
+	void userChange(UserVO vo);
+	
+	// 유저 검색
+	void userSearch(String id);
+	
+	
+	// 유저 비밀번호 변경
+	void userPwChange(String id);
+	
+	// 유저 로그인 유효성 검사
+	int userCheck(String id, String pw);
+	
+	//중복 ID 여부를 검증하는 메서드
+	boolean confirmId(String id);
+	
+}
---
+++ Wow_Family/src/kr/co/wow/UserDAO.java
@@ -0,0 +1,47 @@
+package kr.co.wow;
+
+public class UserDAO implements IUserDAO {
+
+	@Override
+	public void userAdd(String id) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void userDel(String id) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void userChange(UserVO vo) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void userSearch(String id) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void userPwChange(String id) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int userCheck(String id, String pw) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean confirmId(String id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
