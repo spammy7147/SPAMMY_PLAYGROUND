02b3158a65ff0d270f92b4a046a68a1ea85ed881
spammy <spammy7147@gmail.com>
2021-05-01 19:28:31 +0900

    paging & QBE

--- src/test/java/jpa/security/practice/repository/UserRepositoryTest.java
+++ src/test/java/jpa/security/practice/repository/UserRepositoryTest.java
@@ -1,11 +1,17 @@
 package jpa.security.practice.repository;
 
 import jpa.security.practice.domain.User;
+import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.data.domain.ExampleMatcher.GenericPropertyMatchers.contains;
+import static org.springframework.data.domain.ExampleMatcher.GenericPropertyMatchers.endsWith;
 
 @SpringBootTest
 class UserRepositoryTest {
@@ -35,8 +41,40 @@ class UserRepositoryTest {
     void delete(){
 //        userRepository.delete(userRepository.findById(1L).orElseThrow(RuntimeException::new));
         userRepository.deleteById(1L);
+        userRepository.deleteAll(userRepository.findAllById(Lists.newArrayList(1L,3L))); //select 절이 실행되고 entity의 유무를 확인후 삭제
+        userRepository.deleteInBatch(userRepository.findAllById(Lists.newArrayList(1L,3L))); //select 절 없이 바로 eneity의 삭제쿼리를 날리는 방법
+        userRepository.deleteAllInBatch();
 
     }
 
+    @Test
+    void page(){
+        Page<User> users = userRepository.findAll(PageRequest.of(0,3)); //zero-base paging
+
+        System.out.println("page : " + users);
+        System.out.println("totalElements : " + users.getTotalElements());
+        System.out.println("totalPages : " + users.getTotalPages());
+        System.out.println("numberOfElements : " + users.getNumberOfElements());
+        System.out.println("sort : " + users.getSort());
+        System.out.println("size : " + users.getSize());
+
+        users.getContent().forEach(System.out::println);
+    }
+
+    @Test
+    void QBE(){
+//        ExampleMatcher matcher =  ExampleMatcher.matching()
+//                .withIgnorePaths("id")
+//                .withMatcher("name", endsWith());
+//        Example<User> example = Example.of(new User("동"), matcher);
+
+        User user = new User();
+        user.setName("길");
+        ExampleMatcher matcher = ExampleMatcher.matching().withMatcher("name",contains());
+        Example<User> example  = Example.of(user,matcher);
+
+
+        userRepository.findAll(example).forEach(System.out::println);
+    }
 
 }
--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -6,3 +6,13 @@ insert into user(`id`,`name`) values (2, '김뽀삐');
 
 call next value for hibernate_sequence;
 insert into user(`id`,`name`) values (3, '김길동');
+
+call next value for hibernate_sequence;
+insert into user(`id`,`name`) values (4, '덕배');
+
+call next value for hibernate_sequence;
+insert into user(`id`,`name`) values (5, '김춘배');
+
+call next value for hibernate_sequence;
+insert into user(`id`,`name`) values (6, '갑수');
+
