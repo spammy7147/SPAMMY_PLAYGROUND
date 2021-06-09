cf07fccd76876d5b1ab1f78bed055fee00ce7806
spammy <spammy7147@gmail.com>
2021-06-10 00:36:17 +0900

    Home 변경

--- src/main/java/com/seven/jong/controller/SearchController.java
+++ src/main/java/com/seven/jong/controller/SearchController.java
@@ -3,12 +3,14 @@ package com.seven.jong.controller;
 import org.springframework.stereotype.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
+@RequestMapping("/search")
 public class SearchController {
 	
-	@GetMapping("search/searchview")
+	@GetMapping("/searchview")
 	public String searchView() {
 		System.out.println("searchview연결");
 		return "search/searchview";
