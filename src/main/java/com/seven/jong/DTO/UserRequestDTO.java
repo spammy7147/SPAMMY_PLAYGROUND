5a3eb91123976198e99959d39e81266eac34f10e
jian <jianlee.dev@gmail.com>
2021-06-10 21:28:54 +0900

    add user getter setter (lombok error)

--- src/main/java/com/seven/jong/DTO/UserRequestDTO.java
+++ src/main/java/com/seven/jong/DTO/UserRequestDTO.java
@@ -20,4 +20,37 @@ public class UserRequestDTO {
     private String name;
     private String birth;
     private Integer phone;
+    
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getBirth() {
+		return birth;
+	}
+	public void setBirth(String birth) {
+		this.birth = birth;
+	}
+	public Integer getPhone() {
+		return phone;
+	}
+	public void setPhone(Integer phone) {
+		this.phone = phone;
+	}
+    
+    
 }
