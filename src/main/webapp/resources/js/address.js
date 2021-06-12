4e5ec22e757d62469d95b65c77773cf82f17dacb
spammy <spammy7147@gmail.com>
2021-06-12 18:55:58 +0900

    카카오 맵 api 사용

--- src/main/webapp/resources/js/address.js
+++ src/main/webapp/resources/js/address.js
@@ -1,19 +1,16 @@
-type="text/javascript"
-src="//dapi.kakao.com/v2/maps/sdk.js?appkey=72674637f140df9a7167e5cf81c5912b&libraries=services"
-
-    let container = document.getElementById('map');
-    let options = {
+let container = document.getElementById('map');
+let options = {
     center: new kakao.maps.LatLng(33.450701, 126.570667),
     level: 3
 };
 
-    let map = new kakao.maps.Map(container, options);
+let map = new kakao.maps.Map(container, options);
 
-    // 주소-좌표 변환 객체를 생성합니다
-    let geocoder = new kakao.maps.services.Geocoder();
+// 주소-좌표 변환 객체를 생성합니다
+let geocoder = new kakao.maps.services.Geocoder();
 
-    // 주소로 좌표를 검색합니다
-    geocoder.addressSearch('단성사', function(result, status) {
+// 주소로 좌표를 검색합니다
+geocoder.addressSearch('단성사', function(result, status) {
 
     // 정상적으로 검색이 완료됐으면
     if (status === kakao.maps.services.Status.OK) {
@@ -24,15 +21,15 @@ src="//dapi.kakao.com/v2/maps/sdk.js?appkey=72674637f140df9a7167e5cf81c5912b&lib
     let marker = new kakao.maps.Marker({
     map: map,
     position: coords
-});
+    });
 
     // 인포윈도우로 장소에 대한 설명을 표시합니다
     let infowindow = new kakao.maps.InfoWindow({
     content: '<div style="width:150px;text-align:center;padding:6px 0;">숙소</div>'
-});
+    });
     infowindow.open(map, marker);
 
     // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
     map.setCenter(coords);
-}
+    }
 });
