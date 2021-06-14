92f61981b188daac56370b71a692375b92020fdd
jian <jianlee.dev@gmail.com>
2021-06-14 20:10:12 +0900

    modify userInfo part

--- src/main/java/com/seven/jong/service/AdminUserService.java
+++ src/main/java/com/seven/jong/service/AdminUserService.java
@@ -6,6 +6,6 @@ public interface AdminUserService {
 	
 	public void pageUserInfo(int start, Model model);
 	
-	public void info(int member_id, Model model);
+	public void info(int userId, Model model);
 	
 }
