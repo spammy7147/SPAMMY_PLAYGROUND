42bfb66d9ad891bbde04b4965def3a97760aca5e
jeongwoo <dahlia11444@gmail.com>
2021-06-14 20:42:52 +0900

    boardAllList 추가

--- src/main/java/com/seven/jong/controller/BoardContriller.java
+++ src/main/java/com/seven/jong/controller/BoardContriller.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,18 +18,38 @@ public class BoardContriller {
 	@Autowired 
 	BoardService bs;
 	
+	//boardAllList.jsp연결
 	@GetMapping("/boardAllList")
-	public String boardAllList() {
+	public String boardAllList(Model model) {
 		System.out.println("boardAllList연결");
+		bs.boardAllList(model);
 		return "board/boardAllList";
 	}
+	//writeForm.jsp 연결
 	@GetMapping("/writeForm")
 	public String writeForm() {
 		return "board/writeForm";
 	}
+	//게시물 저장
 	@PostMapping("/writeSave")
 	public String writeSave(BoardDTO dto, HttpServletRequest request) {
 		bs.writeSave(dto, request);
 		return "board/boardAllList";
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
