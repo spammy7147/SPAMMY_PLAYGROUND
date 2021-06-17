8831dbae131463e81cb8b1a6a11e0989e5b95881
jian <jianlee.dev@gmail.com>
2021-06-17 20:50:44 +0900

    add admin CS page & make FAQ page

---
+++ src/main/java/com/seven/jong/service/CsServiceImpl.java
@@ -0,0 +1,24 @@
+package com.seven.jong.service;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+
+import com.seven.jong.DTO.FaqDTO;
+import com.seven.jong.repository.ICsMapper;
+
+@Service
+public class CsServiceImpl implements CsService{
+
+	@Autowired ICsMapper mapper;
+	
+	@Override
+	public ArrayList<FaqDTO> faq(Model model) {
+		ArrayList<FaqDTO> faqList = mapper.faqList();
+		model.addAttribute("faqList", faqList);
+		return null;
+	}
+
+}
