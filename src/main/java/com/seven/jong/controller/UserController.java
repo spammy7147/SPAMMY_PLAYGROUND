70c860bff75ee25fabd8fdbfb2f728ad2c9a4c37
spammy <spammy7147@gmail.com>
2021-06-18 16:15:01 +0900

    관리자 권한 계정 테스트

--- src/main/java/com/seven/jong/controller/UserController.java
+++ src/main/java/com/seven/jong/controller/UserController.java
@@ -34,19 +34,24 @@ public class UserController {
 		userService.addUser(user);
 		return "user/register";
 	}
+	@GetMapping("/login")
+	public String login() {
+		System.out.println("login => GET 요청");
+		return "user/login";
+	}
 
-	@RequestMapping("/login")
-	public String login(HttpServletRequest request) {
-		System.out.println("login => 요청");
+	@PostMapping("/login")
+	public void login(HttpServletRequest request) {
+		System.out.println("login => POST 요청");
 
-		String uri = request.getHeader("Referer");
+		String uri = request.getHeader("Referer"); //결국 레퍼러의 값은 이 페이지를 요청한 이전 페이지가 무엇인지를 알려준다. 레퍼러는 로그분석을 위해서 유저의 자취를 분석하는데 요긴한 정보이다.
+
 		if(uri != null){
 			if (!uri.contains("/user/login")) {
 				request.getSession().setAttribute("prevPage", uri);
 			}
 		}
 		System.out.println("Referer : " + uri);
-		return "user/login";
 	}
 
 	@PostMapping("/logout")
