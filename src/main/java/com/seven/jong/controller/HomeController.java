c605ad8376c3ba43f3ac5bf4d214d85e71cc14e9
spammy <spammy7147@gmail.com>
2021-06-18 16:15:01 +0900

    관리자 권한 계정 테스트

--- src/main/java/com/seven/jong/controller/HomeController.java
+++ src/main/java/com/seven/jong/controller/HomeController.java
@@ -2,7 +2,9 @@ package com.seven.jong.controller;
 
 import com.seven.jong.VO.UserVO;
 import com.seven.jong.VO.security.UserSecurityVO;
+import org.springframework.lang.Nullable;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,11 +15,11 @@ public class HomeController {
 
 	@GetMapping("/")
     public String home() {
-        System.out.println("main 진입");
+	    System.out.println("main 진입");
         return "home";
     }
     @GetMapping("/test")
-    public String test(Authentication authentication) {
+    public String test(@Nullable Authentication authentication) {
         System.out.println("/test 요청 들어옴");
         UserSecurityVO userSecurityVO = (UserSecurityVO) authentication.getPrincipal();
         System.out.println("권한:" + authentication.getAuthorities());
