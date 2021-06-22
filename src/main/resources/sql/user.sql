8a150bce65a13bd59754795530753943d444e899
spammy <spammy7147@gmail.com>
2021-06-22 10:46:26 +0900

    file upload test

--- src/main/resources/sql/user.sql
+++ src/main/resources/sql/user.sql
@@ -21,9 +21,17 @@ CREATE SEQUENCE air_user_seq
 ALTER TABLE air_role
     ADD CONSTRAINT FK_role
         FOREIGN KEY (user_id)
-            REFERENCES air_user (user_id);
+            REFERENCES air_user (user_id)
+                ON DELETE CASCADE;
 
 ALTER TABLE hosting
     ADD CONSTRAINT FK_hosting
         FOREIGN KEY (user_id)
-            REFERENCES air_user (user_id);
+            REFERENCES air_user (user_id)
+                ON DELETE CASCADE;
+
+ALTER TABLE persistent
+    ADD CONSTRAINT FK_persistent
+        FOREIGN KEY (user_id)
+            REFERENCES air_user (user_id)
+                ON DELETE CASCADE;
--- src/main/webapp/WEB-INF/views/user/register.jsp
+++ src/main/webapp/WEB-INF/views/user/register.jsp
@@ -1,11 +1,12 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 <%@taglib uri="http://java.sun.com/jsp/jstl/core"  prefix="c"%>
-
+<head>
+	<title>회원가입 - AirBnD</title>
+</head>
 <c:import url="../include/header.jsp" />
 
-<h3>회원가입창 입니다.</h3>
 <div>
-	<form action="<c:url value='/user/register'/>" method="post">
+	<form action="${contextPath}/user/register" method="post">
 		<input type="hidden" name="${_csrf.parameterName}" value="${_csrf.token}" />
 		<input type="text" name="email" placeholder="email"><br>
 		<input type="password" name="password" placeholder="password"><br>
