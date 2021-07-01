69be0e59bba7ca7459d441018e99104fa4b01b39
jian <jianlee.dev@gmail.com>
2021-07-01 19:46:05 +0900

    add admin houseSearch function

--- src/main/java/com/seven/jong/repository/hosting/IAccommodationMapper.java
+++ src/main/java/com/seven/jong/repository/hosting/IAccommodationMapper.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import org.apache.ibatis.annotations.Param;
 
 import com.seven.jong.DTO.hosting.AccommodationDTO;
+import com.seven.jong.VO.UserVO;
 import com.seven.jong.VO.hosting.AccommodationVO;
 
 public interface IAccommodationMapper {
@@ -16,6 +17,12 @@ public interface IAccommodationMapper {
 
  	// 숙소 리스트 가져오기
  	public ArrayList<AccommodationDTO> houseList(@Param("s") int start, @Param("e") int end);
+
+ 	// 조건에 맞는 숙소 수 얻어오기
+ 	public int selectSearchHouseCount(@Param("c")String c, @Param("search")String search);
+
+	//검색 숙소 가져오기
+ 	public ArrayList<AccommodationDTO> houseSearchList(@Param("s")int start, @Param("e")int end, @Param("c")String c, @Param("search")String search);
 
 	
 }
--- src/main/java/com/seven/jong/service/HouseServiceImpl.java
+++ src/main/java/com/seven/jong/service/HouseServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 
 import com.seven.jong.DTO.hosting.AccommodationDTO;
+import com.seven.jong.VO.UserVO;
 import com.seven.jong.repository.hosting.IAccommodationMapper;
 
 @Service
@@ -14,6 +15,7 @@ public class HouseServiceImpl implements HouseService{
 
 	@Autowired IAccommodationMapper aMapper;
 	
+	//숙소 리스트
 	@Override
 	public void houseList(int pageNum, Model model) {
 		int allCount = aMapper.selectHouseCount(); // 총 숙소수 얻어오기
@@ -31,5 +33,28 @@ public class HouseServiceImpl implements HouseService{
 		model.addAttribute("houseList", houseList);
 		
 	}
+	
+	//숙소 검색
+	@Override
+	public void houseSearch(int pageNum, String c, String search, Model model) {
+		int allCount = aMapper.selectSearchHouseCount(c,search); // 조건에 맞는 숙소 수 얻어오기
+		int pageLetter = 10; //한 페이지에 표현 할 갯수
+		int totalPage = allCount /pageLetter; //총 페이지
+		if(allCount % pageLetter != 0) {
+			totalPage += 1;
+		}
+		int end = pageNum * pageLetter;
+		int start = end + 1 - pageLetter;
+		
+		//start,end,컬럼이름,검색내용
+		ArrayList<AccommodationDTO> houseSearchList = aMapper.houseSearchList(start,end,c,search);
+		
+		model.addAttribute("allPage", totalPage);
+		model.addAttribute("houseSearchList", houseSearchList);
+		
+	}
+	
+	
+	
 
 }
