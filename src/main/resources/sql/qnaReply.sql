2582a42b1afb53ee746e72bd7c707769e8679c53
jian <jianlee.dev@gmail.com>
2021-07-02 23:35:56 +0900

    add board & qna fK key

--- src/main/resources/sql/qnaReply.sql
+++ src/main/resources/sql/qnaReply.sql
@@ -5,3 +5,16 @@ write_group number(10),
 write_date date default sysdate,
 constraint qna_rep_test foreign key(write_group) references QNA(qna_no) on delete cascade
 );
+
+ALTER TABLE QNAREPLY
+    ADD CONSTRAINT FK_QNA_reply
+    FOREIGN KEY (email)
+    REFERENCES air_user (email)
+    ON DELETE CASCADE;
+
+
+ALTER TABLE QNAREPLY
+    ADD CONSTRAINT FK_QNA_group
+    FOREIGN KEY (write_group)
+    REFERENCES QNA (qna_no)
+    ON DELETE CASCADE;
