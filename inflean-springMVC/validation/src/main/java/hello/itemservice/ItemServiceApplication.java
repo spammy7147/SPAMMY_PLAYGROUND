4803f0aa04b913cc5fd312a2c3bb34b4efd01768
spammy7147 <spammy7147@gmail.com>
2022-10-03 17:39:23 +0900

    validation done

--- inflean-springMVC/validation/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ inflean-springMVC/validation/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -1,7 +1,10 @@
 package hello.itemservice;
 
+import hello.itemservice.web.validation.ItemValidator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.validation.Validator;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ItemServiceApplication {
@@ -10,4 +13,8 @@ public class ItemServiceApplication {
 		SpringApplication.run(ItemServiceApplication.class, args);
 	}
 
+//	@Override
+//	public Validator getValidator() {
+//		return new ItemValidator();
+//	}
 }
