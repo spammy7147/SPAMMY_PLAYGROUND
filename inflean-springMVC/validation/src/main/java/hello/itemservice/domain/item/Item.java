46e1a1cf04ded5f33c034d25ce4744edebb834c4
spammy7147 <spammy7147@gmail.com>
2022-10-31 01:00:51 +0900

    validation

--- inflean-springMVC/validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ inflean-springMVC/validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -2,7 +2,6 @@ package hello.itemservice.domain.item;
 
 import lombok.Data;
 import org.hibernate.validator.constraints.Range;
-import org.hibernate.validator.constraints.ScriptAssert;
 
 import javax.validation.constraints.Max;
 import javax.validation.constraints.NotBlank;
@@ -12,13 +11,14 @@ import javax.validation.constraints.NotNull;
 //@ScriptAssert(lang="javascript", script = "_this.price * _this.quantity >= 10000", message = "총합이 10000원 넘게 입력해주세요")
 public class Item {
 
+    @NotNull(groups = UpdateCheck.class)
     private Long id;
-    @NotBlank
+    @NotBlank(groups = {SaveCheck.class, UpdateCheck.class})
     private String itemName;
-    @NotNull
-    @Range(min = 1000, max = 1000000)
+    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+    @Range(min = 1000, max = 1000000, groups = {SaveCheck.class, UpdateCheck.class})
     private Integer price;
-    @NotNull
+    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
     @Max(9999)
     private Integer quantity;
 
