899ba3603715ddc528f780f945a19fa4a00c3024
spammy7147 <spammy7147@gmail.com>
2022-11-07 01:57:22 +0900

    validation  DTO 분할, validation 끝

--- inflean-springMVC/validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ inflean-springMVC/validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -11,15 +11,18 @@ import javax.validation.constraints.NotNull;
 //@ScriptAssert(lang="javascript", script = "_this.price * _this.quantity >= 10000", message = "총합이 10000원 넘게 입력해주세요")
 public class Item {
 
-    @NotNull(groups = UpdateCheck.class)
+//    @NotNull(groups = UpdateCheck.class)
     private Long id;
-    @NotBlank(groups = {SaveCheck.class, UpdateCheck.class})
+
+//    @NotBlank(groups = {SaveCheck.class, UpdateCheck.class})
     private String itemName;
-    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
-    @Range(min = 1000, max = 1000000, groups = {SaveCheck.class, UpdateCheck.class})
+//    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+//    @Range(min = 1000, max = 1000000, groups = {SaveCheck.class, UpdateCheck.class})
+
     private Integer price;
-    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
-    @Max(9999)
+//    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+//    @Max(9999)
+
     private Integer quantity;
 
     public Item() {
