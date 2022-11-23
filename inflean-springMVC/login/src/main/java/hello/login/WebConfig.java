83c1eeeec0e51508a049842119f72d8a6b4179ce
spammy7147 <spammy7147@gmail.com>
2022-11-24 00:36:41 +0900

    interceptor done | arguementResolver with custom  annotaion

--- inflean-springMVC/login/src/main/java/hello/login/WebConfig.java
+++ inflean-springMVC/login/src/main/java/hello/login/WebConfig.java
@@ -1,19 +1,27 @@
 package hello.login;
 
+import hello.login.web.argumentResolver.LoginMemberArgumentResolver;
 import hello.login.web.filter.LogFilter;
 import hello.login.web.filter.LoginCheckFilter;
 import hello.login.web.interceptor.LogInterceptor;
+import hello.login.web.interceptor.LoginCheckInterceptor;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.servlet.Filter;
+import java.util.List;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new LoginMemberArgumentResolver());
+    }
 
     /**
      * ? 한 문자 일치
@@ -44,7 +52,13 @@ public class WebConfig implements WebMvcConfigurer {
                 .addPathPatterns("/**")
                 .excludePathPatterns("/css/**", "/*.ico", "/error");
 
+        registry.addInterceptor(new LoginCheckInterceptor())
+                .order(2)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/", "/members/add", "/login", "logout",
+                        "/css/**", "/*.ico", "/error");
     }
+
 
 //    @Bean
 //    public FilterRegistrationBean logFilter() {
