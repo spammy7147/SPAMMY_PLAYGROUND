cf2cfd3765c1abf334339dc11846ea14b289018f
spammy7147 <spammy7147@gmail.com>
2022-12-19 21:29:57 +0900

    인터셉터 & spring  기능

--- inflean-springMVC/exception/src/main/java/hello/exception/servlet/ServletExController.java
+++ inflean-springMVC/exception/src/main/java/hello/exception/servlet/ServletExController.java
@@ -18,6 +18,9 @@ public class ServletExController {
     @GetMapping("/error-404")
     public void error404(HttpServletResponse response) throws IOException {
         response.sendError(404, "404 오류!");
+    }    @GetMapping("/error-400")
+    public void error400(HttpServletResponse response) throws IOException {
+        response.sendError(400, "400 오류!");
     }
 
     @GetMapping("/error-500")
