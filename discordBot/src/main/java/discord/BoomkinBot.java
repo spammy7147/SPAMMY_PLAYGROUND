2e10a638bd3639b234bc92ba419d54ddd690da56
spammy7147 <spammy7147@gmail.com>
2022-12-21 00:32:25 +0900

    discord bot with spring boot

---
+++ discordBot/src/main/java/discord/BoomkinBot.java
@@ -0,0 +1,14 @@
+package discord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoomkinBot {
+    public static void main(String[] args) {
+        SpringApplication.run(BoomkinBot.class, args);
+    }
+
+
+
+}
---
+++ discordBot/src/main/java/discord/bot/BotToken.java
@@ -0,0 +1,6 @@
+package discord.bot;
+
+public interface BotToken {
+
+    String DISCORD_BOT_TOKEN = "ODExMDk4NDQwNjExNzkwODc4.Gb08u8.MKk8ucocnju7Ezh-0LamoSnkYbzprAo2nyAGRw";
+}
