356739da3787052a039213bec777518ddf44cb38
spammy <spammy7147@gamil.com>
2023-03-26 21:38:33 +0900

    Connection Pool - hikariCp

--- inflean-springDB/jdbc/src/test/java/hello/jdbc/connection/ConnectionTest.java
+++ inflean-springDB/jdbc/src/test/java/hello/jdbc/connection/ConnectionTest.java
@@ -1,5 +1,6 @@
 package hello.jdbc.connection;
 
+import com.zaxxer.hikari.HikariDataSource;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -30,6 +31,20 @@ public class ConnectionTest {
 
         DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
         userDataSource(dataSource);
+    }
+
+    @Test
+    void dataSourceConnectionPool() throws SQLException, InterruptedException {
+        //커넥션 풀링
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+        dataSource.setMaximumPoolSize(10);
+        dataSource.setPoolName("MyPool");
+
+        userDataSource(dataSource);
+        Thread.sleep(1000);
     }
 
     private void userDataSource(DataSource dataSource) throws SQLException {
