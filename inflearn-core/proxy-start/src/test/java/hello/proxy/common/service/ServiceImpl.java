19304172ad7bf8dc8039a96e38371d400b34cec5
spammy <spammy7147@gamil.com>
2024-04-07 21:17:11 +0900

    JDK 동적 프록시 & CGLIB 알아보기

---
+++ inflearn-core/proxy-start/src/test/java/hello/proxy/common/service/ServiceImpl.java
@@ -0,0 +1,13 @@
+package hello.proxy.common.service;
+
+public class ServiceImpl implements ServiceInterface {
+    @Override
+    public void save() {
+
+    }
+
+    @Override
+    public void find() {
+
+    }
+}
