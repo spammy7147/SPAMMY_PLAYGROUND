007953a4184883eab7f45fb995fb8c123143062c
spammy <spammy7147@gamil.com>
2024-07-09 00:03:28 +0900

    java

---
+++ inflearn-java/src/collection/link/NodeMain1.java
@@ -0,0 +1,26 @@
+package collection.link;
+
+import java.util.ArrayList;
+
+public class NodeMain1 {
+
+    public static void main(String[] args) {
+        //노드 생성하고 연결하기 : A -> B -> C
+
+        Node first = new Node("A");
+        first.next = new Node("B");
+        first.next.next = new Node("C");
+
+        System.out.println("모든 노드 탐색하기");
+        System.out.println("first.item = " + first.item);
+        System.out.println("first.next.item = " + first.next.item);
+        System.out.println("first.next.next.item = " + first.next.next.item);
+
+        Node x = first;
+        while (x != null) {
+            System.out.println(x.item);
+            x = x.next;
+        }
+
+    }
+}
