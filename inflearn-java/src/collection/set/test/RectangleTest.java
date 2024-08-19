a8081d2203d3bdfd1be9c0b67ceb1c7e7f944fde
spammy <spammy7147@gamil.com>
2024-08-19 22:25:58 +0900

    hashTest example problem

---
+++ inflearn-java/src/collection/set/test/RectangleTest.java
@@ -0,0 +1,17 @@
+package collection.set.test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class RectangleTest {
+    public static void main(String[] args) {
+        Set<Rectangle> rectangles = new HashSet<>();
+        rectangles.add(new Rectangle(10, 10));
+        rectangles.add(new Rectangle(20, 20));
+        rectangles.add(new Rectangle(20, 20));
+
+        for(Rectangle rectangle : rectangles) {
+            System.out.println("rectangle = " + rectangle);
+        }
+    }
+}
