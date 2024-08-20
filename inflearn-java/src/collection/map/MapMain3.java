1fc4086adfba4a09190e9bb5fb4a00135a583b3e
spammy <spammy7147@gamil.com>
2024-08-20 22:42:25 +0900

    map

---
+++ inflearn-java/src/collection/map/MapMain3.java
@@ -0,0 +1,24 @@
+package collection.map;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+
+public class MapMain3 {
+    public static void main(String[] args) {
+        Map<String, Integer> studentMap = new HashMap<>();
+
+        studentMap.put("studentA", 50);
+        System.out.println("studentMap = " + studentMap);
+
+        //학생이 없는 경우에만 추가1
+        if (!studentMap.containsKey("studentA")) {
+            studentMap.put("studentA", 100);
+        }
+        System.out.println("studentMap = " + studentMap);
+
+        studentMap.putIfAbsent("studentA", 100);
+        studentMap.putIfAbsent("studentB", 100);
+        System.out.println("studentMap = " + studentMap);
+    }
+}
