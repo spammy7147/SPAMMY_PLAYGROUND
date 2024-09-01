de40b6b33c9d29faa8fb365f56606ea83b6dd2d8
spammy <spammy7147@gamil.com>
2024-09-01 21:48:16 +0900

    queue 문제풀이

--- inflearn-java/src/collection/deque/test/queue/task/BackupTask.java
+++ inflearn-java/src/collection/deque/test/queue/task/BackupTask.java
@@ -3,6 +3,6 @@ package collection.deque.test.queue.task;
 public class BackupTask implements Task{
     @Override
     public void execute() {
-
+        System.out.println("자료 백업...");
     }
 }
--- inflearn-java/src/collection/deque/test/queue/task/CompressionTask.java
+++ inflearn-java/src/collection/deque/test/queue/task/CompressionTask.java
@@ -3,6 +3,6 @@ package collection.deque.test.queue.task;
 public class CompressionTask implements Task{
     @Override
     public void execute() {
-
+        System.out.println("데이터 압축...");
     }
 }
