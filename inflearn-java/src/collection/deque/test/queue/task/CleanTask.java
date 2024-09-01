e299ee463d62eca22b5d8a839139d175fd275a78
spammy <spammy7147@gamil.com>
2024-09-01 21:48:16 +0900

    queue 문제풀이

--- inflearn-java/src/collection/deque/test/queue/task/CleanTask.java
+++ inflearn-java/src/collection/deque/test/queue/task/CleanTask.java
@@ -3,6 +3,6 @@ package collection.deque.test.queue.task;
 public class CleanTask implements Task{
     @Override
     public void execute() {
-
+        System.out.println("사용하지 않는 자원 정리...");
     }
 }
