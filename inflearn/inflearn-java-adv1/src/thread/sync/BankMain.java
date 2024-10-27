6d1671a1819efb83a110f132c71753083b6f4d65
spammy <spammy7147@gmail.com>
2024-10-27 18:31:01 +0900

    lock

--- inflearn/inflearn-java-adv1/src/thread/sync/BankMain.java
+++ inflearn/inflearn-java-adv1/src/thread/sync/BankMain.java
@@ -5,7 +5,10 @@ import static util.ThreadUtils.sleep;
 
 public class BankMain {
     public static void main(String[] args) throws InterruptedException {
-        BankAccountV1 account = new BankAccountV1(1000);
+//        BankAccountV3 account = new BankAccountV3(1000);
+//        BankAccountV4 account = new BankAccountV4(1000);
+//        BankAccountV5 account = new BankAccountV5(1000);
+        BankAccountV6 account = new BankAccountV6(1000);
 
         Thread t1 = new Thread(new WithdrawTask(account, 800), "t1");
         Thread t2 = new Thread(new WithdrawTask(account, 800), "t2");
@@ -15,7 +18,7 @@ public class BankMain {
 
         sleep(500);
         log("t1 state: " + t1.getState());
-        log("t1 state: " + t2.getState());
+        log("t2 state: " + t2.getState());
 
         t1.join();
         t2.join();
