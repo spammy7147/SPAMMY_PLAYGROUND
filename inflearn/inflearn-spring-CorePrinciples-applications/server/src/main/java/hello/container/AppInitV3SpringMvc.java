251df59e56df727aa1785c13c38fa90dbce36d4b
spammy <spammy7147@gmail.com>
2025-01-21 22:29:55 +0900

    인프런

--- inflearn/inflearn-spring-CorePrinciples-applications/server/src/main/java/hello/container/AppInitV3SpringMvc.java
+++ inflearn/inflearn-spring-CorePrinciples-applications/server/src/main/java/hello/container/AppInitV3SpringMvc.java
@@ -5,7 +5,6 @@ import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRegistration;
 import org.springframework.web.WebApplicationInitializer;
-import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
