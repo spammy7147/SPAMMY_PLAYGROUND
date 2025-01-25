870fcc255ac0b080e2c8931581e2d8f2b59c49c8
spammy7147 <spammy7147@gmail.com>
2025-01-25 21:26:13 +0900

    autoConfigure

---
+++ inflearn/inflearn-spring-CorePrinciples-applications/memory-v1/src/main/java/memory/MemoryController.java
@@ -0,0 +1,27 @@
+package memory;
+
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+public class MemoryController {
+
+    private final MemoryFinder memoryFinder;
+
+    @GetMapping("/memory")
+    public Memory system() {
+        Memory memory = memoryFinder.get();
+        log.info("memory={}", memory);
+        return memory;
+    }
+
+    @PostConstruct
+    public void init() {
+        log.info("init memoryController");
+    }
+}
