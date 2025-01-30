9b5bf359889d30ad651acc61cb7362d587808776
spammy <spammy7147@gmail.com>
2025-01-30 10:09:27 +0900

    @Value

--- inflearn/inflearn-spring-CorePrinciples-applications/external-read/src/main/java/hello/ExternalReadApplication.java
+++ inflearn/inflearn-spring-CorePrinciples-applications/external-read/src/main/java/hello/ExternalReadApplication.java
@@ -1,12 +1,14 @@
 package hello;
 
 import hello.config.MyDataSourceEnvConfig;
+import hello.config.MyDataValueConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
 
-@Import(MyDataSourceEnvConfig.class)
+//@Import(MyDataSourceEnvConfig.class)
+@Import(MyDataValueConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.datasource")
 public class ExternalReadApplication {
 
