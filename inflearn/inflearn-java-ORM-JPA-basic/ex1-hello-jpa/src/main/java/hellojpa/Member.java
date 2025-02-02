e7da1bf5b13b6b59b6b46e0a6ad39663aaafd12b
spammy <spammy7147@gmail.com>
2025-02-03 00:40:24 +0900

    다양한 연관관계 매핑

--- inflearn/inflearn-java-ORM-JPA-basic/ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ inflearn/inflearn-java-ORM-JPA-basic/ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -15,8 +15,9 @@ public class Member {
     @JoinColumn(name = "TEAM_ID")
     private Team team;
 
-//    @Column(name="TEAM_ID")
-//    private Long teamId;
+    @OneToOne
+    @JoinColumn(name ="LOCKER_ID")
+    private Locker locker;
 
     public Long getId() {
         return id;
@@ -38,12 +39,15 @@ public class Member {
         return team;
     }
 
-    public void changeTeam(Team team) {
-        this.team = team;
-        team.getMembers().add(this);
-    }
-
     public void setTeam(Team team) {
         this.team = team;
+    }
+
+    public Locker getLocker() {
+        return locker;
+    }
+
+    public void setLocker(Locker locker) {
+        this.locker = locker;
     }
 }
