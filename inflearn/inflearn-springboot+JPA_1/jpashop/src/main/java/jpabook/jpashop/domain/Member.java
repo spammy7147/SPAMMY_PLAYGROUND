407c5b01be747302847f1b0d36246f296ce7aa1e
spammy <spammy7147@gmail.com>
2025-02-13 01:01:13 +0900

    도메인 분석 설계

--- inflearn/inflearn-springboot+JPA_1/jpashop/src/main/java/jpabook/jpashop/domain/Member.java
+++ inflearn/inflearn-springboot+JPA_1/jpashop/src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,17 +1,29 @@
 package jpabook.jpashop.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.aspectj.weaver.ast.Or;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
 @Setter
 public class Member {
     @Id @GeneratedValue
+    @Column(name = "member_id")
     private Long id;
 
     private String username;
+
+    @Embedded
+    private Address address;
+
+    @OneToMany(mappedBy = "member")
+    private List<Order> orders = new ArrayList<>();
+
+
+
 }
---
+++ inflearn/inflearn-springboot+JPA_1/jpashop/src/main/java/jpabook/jpashop/domain/item/Book.java
@@ -0,0 +1,17 @@
+package jpabook.jpashop.domain.item;
+
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+@DiscriminatorValue("B")
+public class Book extends Item {
+
+    private String author;
+    private String isbn;
+
+}
