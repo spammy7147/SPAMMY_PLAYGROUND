861bef577af73ab3395b3a7d83221511f1f5c971
spammy <spammy7147@gmail.com>
2025-02-14 00:41:04 +0900

    회원 도메인 개발

--- inflearn/inflearn-springboot+JPA_1/jpashop/src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ inflearn/inflearn-springboot+JPA_1/jpashop/src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -1,15 +1,17 @@
 package jpabook.jpashop.repository;
 
 import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
 import jpabook.jpashop.domain.Member;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
+@RequiredArgsConstructor
 public class MemberRepository {
 
-    @PersistenceContext
-    private EntityManager em;
+    private final EntityManager em;
 
     public Long save(Member member) {
         em.persist(member);
@@ -18,6 +20,17 @@ public class MemberRepository {
 
     public Member find(Long id) {
         return em.find(Member.class, id);
+    }
+
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+
+    public List<Member> findByName(String name) {
+        return em.createQuery("select m from Member m where m.name = :name", Member.class)
+                .setParameter("name", name)
+                .getResultList();
     }
 
 }
