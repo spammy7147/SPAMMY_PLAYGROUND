3bfa258792dadb012095b52ebbe42cebf54e007c
spammy <spammy7147@gmail.com>
2025-02-16 20:22:27 +0900

    주문 도메인 개발

---
+++ inflearn/inflearn-springboot+JPA_1/jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -0,0 +1,30 @@
+package jpabook.jpashop.repository;
+
+import jakarta.persistence.EntityManager;
+import jpabook.jpashop.domain.Order;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepository {
+
+    private final EntityManager em;
+
+    public void save(Order order) {
+        em.persist(order);
+    }
+
+    public Order find(Long orderId) {
+        return em.find(Order.class, orderId);
+    }
+
+
+//    public List<Order> findAll(OrderSearch orderSearch) {
+//        return em.createQuery("select o from Order o", Order.class)
+//                .getResultList();
+//    }
+
+}
