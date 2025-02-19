713a8d65cd4280ecbd37b7f9ba65aa2ad6e8f870
spammy <spammy7147@gmail.com>
2025-02-19 23:08:05 +0900

    api 개발 기본 회원등록api

---
+++ inflearn/inflearn-springboot+JPA_2/jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -0,0 +1,49 @@
+package jpabook.jpashop.domain.item;
+
+import jakarta.persistence.*;
+import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.domain.exception.NotEnoughStockException;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "dtype")
+public abstract class Item {
+    @Id @GeneratedValue
+    @Column(name = "item_id")
+    private Long id;
+
+    private String name;
+    private int price;
+    private int stockQuantity;
+
+    @ManyToMany(mappedBy = "items")
+    private List<Category> categories = new ArrayList<>();
+
+//== 비즈니스 로직 시작 ==//
+    /**
+     * stock 증가
+     * @param quantity
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * stock 감소
+     */
+    public void removeStock(int quantity) {
+        int resultStock = this.stockQuantity - quantity;
+        if (resultStock <= 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+
+        this.stockQuantity = resultStock;
+    }
+}
