00c2c3345b5be1c7aaa1f878204aac33d5ac7745
spammy <spammy7147@gmail.com>
2025-02-23 23:09:23 +0900

    최적화 및 OSIV

--- inflearn/inflearn-springboot+JPA_2/jpashop/src/main/java/jpabook/jpashop/repository/order/query/OrderQueryDto.java
+++ inflearn/inflearn-springboot+JPA_2/jpashop/src/main/java/jpabook/jpashop/repository/order/query/OrderQueryDto.java
@@ -3,24 +3,36 @@ package jpabook.jpashop.repository.order.query;
 import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.OrderStatus;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
+@EqualsAndHashCode(of = "orderId")
 public class OrderQueryDto {
 
     private Long orderId;
     private String name;
-    private LocalDateTime orderTime;
+    private LocalDateTime orderDate;
     private OrderStatus orderStatus;
     private Address address;
     private List<OrderItemQueryDto> orderItems;
 
-    public OrderQueryDto(Long orderId, String name, LocalDateTime orderTime, OrderStatus orderStatus, Address address) {
+
+    public OrderQueryDto(Long orderId, String name, LocalDateTime orderTime, OrderStatus orderStatus, Address address, List<OrderItemQueryDto> orderItems) {
         this.orderId = orderId;
         this.name = name;
-        this.orderTime = orderTime;
+        this.orderDate = orderTime;
+        this.orderStatus = orderStatus;
+        this.address = address;
+        this.orderItems = orderItems;
+    }
+
+    public OrderQueryDto(Long orderId, String name, LocalDateTime orderDate, OrderStatus orderStatus, Address address) {
+        this.orderId = orderId;
+        this.name = name;
+        this.orderDate = orderDate;
         this.orderStatus = orderStatus;
         this.address = address;
     }
