5d4340cbe9114ebc74c5bee2f124fec9a73eff67
spammy <spammy7147@gmail.com>
2025-04-13 17:48:18 +0900

    호프 커페 수정

--- hof-custom/script.js
+++ hof-custom/script.js
@@ -1,11 +1,47 @@
+const 전투 = {
+				'' : 'Mine01'
+			}
 
-const e1 = "Equip_L_1"
-const e2 = "Equip_L_2"
+const 모험 = {
+				'뒷산의 광산' : 'Mine01',
+				'뒷산의 숲' : 'Herb01',
+				'천년제 무투회' : 'festival01',
+				'천년제 무투회(HARD)' : 'festival011',
+				'마법신의 탑(상층(裏))' : 'mg06',
+				'천체관' : 'mg04',
+				'태양의 제단' : 'Pyra55',
+				'마을 묘지' : 'Grave001'
+			}
+
 
 function pattern(formId, patternno){
 	const form = document.getElementById(formId);
 	form.querySelector('input[name="patternno"]').value = patternno;
 	form.submit();
+}
+
+
+function party_pattern(class1, pattern1, class2, pattern2, class3, pattern3, class4, pattern4, class5, pattern5) {
+	setTimeout(() => {pattern(class1, pattern1)}, 0)
+	setTimeout(() => {pattern(class2, pattern2)}, 500)
+	setTimeout(() => {pattern(class3, pattern3)}, 1000)
+	setTimeout(() => {pattern(class4, pattern4)}, 1500)
+	setTimeout(() => {pattern(class5, pattern5)}, 2000)
+}
+
+function battle(formId, action) { 
+	const form = document.getElementById(formId);
+	form.action = action
+	form.submit();
+}
+
+function battle_chain(party) {
+	let idx = 0;
+	for(key in 모험) {
+		let action = 'http://sic.zerosic.com/ZeroHOF/index.php?sp_common=' + 모험[key] 
+		setTimeout(() => {battle(party, action)}, idx++ * 1000)
+	}
+
 }
 
 function equip(a,b,c){
@@ -20,13 +56,7 @@ function equip(a,b,c){
 	form.removeChild(input);
 }
 
-function pattern(pattern1, pattern2, pattern3, pattern4, pattern5) {
-	setTimeout(pattern,0,a,b,2);
-	setTimeout(pattern,1000,c,d,3);
-	setTimeout(pattern,2000,e,f,4);
-	setTimeout(pattern,3000,g,h,5);
-	setTimeout(pattern,4000,i,j,6);
-}
+
 
 function u_equip_5(a,b,c,d,e,f,g,h,i,j ) {
 	setTimeout(equip,0,a,b,2);
