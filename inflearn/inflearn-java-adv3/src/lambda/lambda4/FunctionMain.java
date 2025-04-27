619043472daac70ecc1ed51654db2d1f6b3b22fa
spammy <spammy7147@gmail.com>
2025-04-27 21:34:31 +0900

    기본 함수형 인터페이스

--- inflearn/inflearn-java-adv3/src/lambda/lambda4/FunctionMain.java
+++ inflearn/inflearn-java-adv3/src/lambda/lambda4/FunctionMain.java
@@ -1,2 +1,20 @@
-package lambda.lambda4;public class FunctionMain {
+package lambda.lambda4;
+
+import java.util.function.Function;
+
+public class FunctionMain {
+    public static void main(String[] args) {
+        //익명클래스
+        Function<String, Integer> function1 = new Function<String, Integer>() {
+            @Override
+            public Integer apply(String s) {
+                return s.length();
+            }
+        };
+
+        System.out.println("function1.apply(\"hello\") = " + function1.apply("hello"));
+
+        Function<String, Integer> function2 = s -> s.length();
+        System.out.println("function2 = " + function2.apply("hello"));
+    }
 }
