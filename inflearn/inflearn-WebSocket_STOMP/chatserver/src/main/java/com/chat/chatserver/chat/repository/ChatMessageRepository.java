8c058bab0c4b8327b2c3b06a62358d14ac091539
spammy <spammy7147@gmail.com>
2025-04-30 01:25:31 +0900

    메세지 조회

--- inflearn/inflearn-WebSocket_STOMP/chatserver/src/main/java/com/chat/chatserver/chat/repository/ChatMessageRepository.java
+++ inflearn/inflearn-WebSocket_STOMP/chatserver/src/main/java/com/chat/chatserver/chat/repository/ChatMessageRepository.java
@@ -1,9 +1,13 @@
 package com.chat.chatserver.chat.repository;
 
 import com.chat.chatserver.chat.domain.ChatMessage;
+import com.chat.chatserver.chat.domain.ChatRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
+    List<ChatMessage> findByChatRoomOrderByCreatedTimeAsc(ChatRoom chatRoom);
 }
