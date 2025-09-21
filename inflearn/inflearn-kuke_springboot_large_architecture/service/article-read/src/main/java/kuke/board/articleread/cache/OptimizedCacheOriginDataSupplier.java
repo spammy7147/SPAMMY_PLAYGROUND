74ea09da71e7082ffebfe27f155e20078c3ac32d
spammy <spammy7147@gmail.com>
2025-09-21 23:15:15 +0900

    삭제

--- inflearn/inflearn-kuke_springboot_large_architecture/service/article-read/src/main/java/kuke/board/articleread/cache/OptimizedCacheOriginDataSupplier.java
+++
@@ -1,6 +0,0 @@
-package kuke.board.articleread.cache;
-
-@FunctionalInterface
-public interface OptimizedCacheOriginDataSupplier<T> {
-    T get() throws Throwable;
-}
--- inflearn/inflearn-kuke_springboot_large_architecture/service/article-read/src/main/java/kuke/board/articleread/service/event/handler/EventHandler.java
+++
@@ -1,9 +0,0 @@
-package kuke.board.articleread.service.event.handler;
-
-import kuke.board.common.event.Event;
-import kuke.board.common.event.EventPayload;
-
-public interface EventHandler<T extends EventPayload> {
-    void handle(Event<T> event);
-    boolean supports(Event<T> event);
-}
